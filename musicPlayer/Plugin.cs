1a28e373cddd7b294a755eaaa7a8249cb75d7086
Guazzaroni Leo <leo.guazzaroni@epita.fr>
2023-12-01 18:15:28 +0100

    WIP: working on the file paths

--- musicPlayer/Plugin.cs
+++ musicPlayer/Plugin.cs
@@ -1,8 +1,27 @@
-﻿using BepInEx;
+﻿using System.Collections;
+using System.IO;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using musicPlayer.Patcher;
 using UnityEngine;
+using UnityEngine.Networking;
+
+
 namespace musicPlayer
 {
+    
+    public static class ConfigSettings
+    {
+        public static ConfigEntry<string> musicPath;
+
+        public static void BindConfigSettings()
+        {
+            MusicLogger.logger.LogInfo("Binding the settings");
+            musicPath = ((BaseUnityPlugin)Plugin.instance).Config.Bind<string>("musicPlayer", "CustomMusicPath", "music/tellem.wav", "Absolute or local video path. Use forward slashes in your path. Local paths are local to the BepInEx/plugins folder, and should not begin with a slash.");
+        }
+    }
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
@@ -10,14 +29,29 @@ namespace musicPlayer
 
         private Harmony _harmony;
 
+        public static string filePath;
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            instance = this;
+            ConfigSettings.BindConfigSettings();
+            filePath = ConfigSettings.musicPath.Value;
+            if (filePath.Length <= 0 || Enumerable.Contains(filePath, ' '))
+            {
+                filePath = ((ConfigEntryBase)ConfigSettings.musicPath).DefaultValue.ToString();
+            }
+            filePath = filePath.Replace('/', Path.DirectorySeparatorChar);
+            if (!Path.IsPathRooted(filePath))
+            {
+                filePath = Path.Combine(Paths.PluginPath, filePath.TrimStart(new char[1] { '/' }));
+            }
+            Logger.LogInfo("Using configuration from file : " + filePath);
+            
             _harmony = new Harmony("musicPlayer");
             _harmony.PatchAll();
             Logger.LogInfo((object)"MusicPlayer is loaded and Harmony initiated");
-            instance = this;
         }
     }
 }
@@ -34,28 +68,28 @@ namespace musicPlayer.Patcher
     internal class MusicPlayer
     {
         [HarmonyPatch(typeof(BoomboxItem), "StartMusic")]
-        [HarmonyPostfix]
-        public static void StartMusic_Prefix(BoomboxItem __instance,
+        [HarmonyPrefix]
+        public static bool StartMusic_Prefix(BoomboxItem __instance,
             bool startMusic, bool pitchDown = false)
         {
-            MusicLogger.logger.LogInfo("Doing some sound !!");
             if (startMusic)
             {
                 // Replace the audio clip assignment with your custom logic
+                MusicLogger.logger.LogInfo("Doing some sound !!");
+
                 __instance.boomboxAudio.clip = LoadCustomAudioClip();
                 __instance.boomboxAudio.pitch = 1f;
                 __instance.boomboxAudio.Play();
             }
+
+            return false;
         }
 
         private static AudioClip LoadCustomAudioClip()
         {
-            string audioFile = "assets/tellem.wav";
-            
-            WWW www = new WWW("file://" + audioFile);
-            MusicLogger.logger.LogInfo("File : [ " + www +  " ]");
+            WWW www = new WWW($"file://{Plugin.filePath}");
+            MusicLogger.logger.LogInfo(www.url);
             AudioClip audioClip = www.GetAudioClip(false, false, AudioType.WAV);
-            
             return audioClip;
         }
     }
